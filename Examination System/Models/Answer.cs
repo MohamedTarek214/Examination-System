192a5d503ee50b843cc1060ee65e4e5598cb68c5
MhmedTarekRadwan <mhmedtarekradwan@gmail.com>
2026-09-17 14:12:18 +0300

    Add Question and Answer models

---
+++ Examination System/Models/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Answer(int Id, string Text)
+        {
+            this.Id = Id;
+
+            this.Text = Text;
+        }
+    }
+}
---
+++ Examination System/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System.Models
+{
+    public class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+        public Question(string header , string body , int mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+        }
+    }
+}
